803ba2132fa6633c1dc3cd6b490ff5cef22063f9
Ioan <ionutioan16@gmail.com>
2024-02-28 19:34:50 +0200

    lalala

---
+++ Lab 1/PrimuLabOOP.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int string_to_int(char s[])
+{
+    int result = 0;
+    for (int i = 0; s[i] != '\0'; i++)
+        result = result * 10 + (s[i] - '0');
+    return result;
+}
+
+int main() 
+{
+    FILE* file;
+    char line[100];
+    int sum = 0;
+    file = fopen("in.txt", "r");
+
+    if (file == NULL) 
+    {
+        printf("Eroare\n");
+        return 1; 
+    }
+
+    while (fgets(line, sizeof(line), file))
+        sum += string_to_int(line);
+
+    fclose(file);
+    printf("%d\n", sum);
+
+    return 0;
+}
