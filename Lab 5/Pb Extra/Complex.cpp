f7ad998acf4b2f0f3ece9a666463f5562df0302e
Ioan <ionutioan16@gmail.com>
2024-04-08 00:51:23 +0300

    lalala

---
+++ Lab 5/Pb Extra/Complex.cpp
@@ -0,0 +1,142 @@
+#include "complex.h"
+
+using namespace std;
+
+Complex::Complex() : Complex(0, 0) {
+}
+
+Complex::Complex(double real, double imag) {
+    real_data = real;
+    imag_data = imag;
+}
+
+bool Complex::is_real() const {
+    return imag() == 0;
+}
+
+double Complex::real() const {
+    return real_data;
+}
+
+double Complex::imag() const {
+    return imag_data;
+}
+
+double Complex::abs() const {
+    return sqrt(real() * real() + imag() * imag());
+}
+
+Complex Complex::conjugate() const {
+    return { real(), -imag() };
+}
+
+Complex& Complex::operator()(double real, double imag)
+{
+    real_data = real;
+    imag_data = imag;
+    return *this;
+}
+
+Complex& Complex::operator++()
+{
+    real_data++;
+    return *this;
+}
+
+Complex& Complex::operator++(int)
+{
+    real_data++;
+    return *this;
+}
+
+Complex& Complex::operator--()
+{
+    real_data--;
+    return *this;
+}
+
+Complex& Complex::operator--(int)
+{
+    real_data--;
+    return *this;
+}
+
+Complex operator+(const Complex& l, const Complex& r)
+{
+    return Complex(l.real() + r.real(), l.imag() + r.imag());
+}
+
+Complex operator+(const Complex& l, double r)
+{
+    return Complex(l.real() + r, l.imag());
+}
+
+Complex operator+(double l, const Complex& r)
+{
+    return r + l;
+}
+
+Complex operator-(const Complex& l, const Complex& r)
+{
+    return Complex(l.real() - r.real(), l.imag() - r.imag());
+}
+
+Complex operator-(const Complex& l, double r)
+{
+    return Complex(l.real() - r, l.imag());
+}
+
+Complex operator-(double l, const Complex& r)
+{
+    return Complex(l - r.real(), -r.imag());
+}
+
+Complex operator*(const Complex& l, const Complex& r)
+{
+    /// (a + bi) * (c + di) = a * c + a * d i + b * c i - (b * d) )
+                         ///= a * c - (b * d) + (a * d + b * c) i
+    return Complex(l.real() * r.real() - (l.imag() * r.imag()), l.real() * r.imag() + l.imag() * r.real());
+}
+
+Complex operator*(const Complex& l, double r)
+{
+    // (a + bi) * c = a * c + b * c i
+    return Complex(l.real() * r, l.imag() * r);
+}
+
+Complex operator*(double l, const Complex& r)
+{
+    return r * l;
+}
+
+Complex operator-(const Complex& obj)
+{
+    return Complex(-obj.real(), -obj.imag());
+}
+
+bool operator==(const Complex& l, const Complex& r)
+{
+    return (l.real() == r.real() && l.imag() == r.imag());
+}
+
+bool operator!=(const Complex& l, const Complex& r)
+{
+    return !(l == r);
+}
+
+std::ostream& operator<<(std::ostream& out, const Complex& complex)
+{
+    if (complex.real() != 0 || complex.imag() == 0)
+        out << complex.real();
+    if (complex.imag() != 0)
+    {
+        if (complex.imag() > 0 && complex.real() != 0)
+            out << " + ";
+        else if (complex.imag() < 0)
+            out << " - ";
+        if(abs(complex.imag() != 1))
+            out << abs(complex.imag());
+        out << "i";
+    }
+    return out;
+}
